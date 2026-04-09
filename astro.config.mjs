98d74a676d676844dfcb03deed9e8c8be7da7a84
ceej <118633698+Ceej-16@users.noreply.github.com>
2026-04-09 10:36:58 -0400

    Fix missing comma in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,15 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://Ceej-16.github.io'
+	site: 'https://Ceej-16.github.io',
 	integrations: [
 		starlight({
 			title: 'Ceej Cyber Lab',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/Ceej-16' }],
 			sidebar: [
 				{
 					label: 'Guides',
 					items: [
-						// Each item here is one entry in the navigation menu.
 						{ label: 'Example Guide', slug: 'guides/example' },
 					],
 				},
